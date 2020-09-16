e27338d2acfc003bec2ea876bed58db21d109388
KrystynaWaniova <krystyna.waniova@idevband.com>
2020-09-16 21:00:37 +0200

    rm key and refactor

--- src/App.tsx
+++ src/App.tsx
@@ -14,20 +14,12 @@ type RecipeType = {
 };
 
 function App() {
-  const APP_ID = "758939af";
-  const APP_KEY = "adf50208712e80852d0a59199baf50cd";
+  const APP_ID = "abc"; 
+  const APP_KEY = "abc"; //add API id
 
   const [recipes, setRecipes] = useState([]);
   const [search, setSearch] = useState('');
   const [query, setQuery] = useState('banana')
-
-  
-  //const [counter, setCounter] = useState(0);
-  //const [c2, setCounter2] = useState(0);
-
-  // useEffect(() => {
-  //   console.log("Effect has been run.");
-  // }, [counter]); // just run when the counter changes
 
   useEffect(() => {
     console.log(search);
@@ -39,23 +31,16 @@ function App() {
     const data = await response.json();
 
     setRecipes(data.hits);
-    // fetch(exampleRequest)
-    // .then(response => {
-    //   response.json();
-    // })
-    // .then(data => console.log(data));
     console.log(query)
     console.log(data.hits);
   };
 
   const updateSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
       setSearch(e.target.value);
-      //console.log(search);
   };
 
   const getSearch = (e : React.FormEvent<HTMLButtonElement>) => {
     e.preventDefault();
-    //console.log(search + "search");
     setQuery(search);
     setSearch("");
   }
@@ -67,8 +52,6 @@ function App() {
         <button onClick={getSearch} className="search-button" type="submit">
           Search
         </button>
-        {/* <h1 onClick={() => setCounter(counter + 1)}>{counter}</h1>
-  <h1 onClick={() => setCounter2(c2 + 1)}>{c2}</h1> */}
       </form>
       <div className="recipes">
       {recipes.map((recipee: RecipeType) => (
